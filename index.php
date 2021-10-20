028d96827d352a75d1300923246f8fc7e4c4bed8
Zarzuket <davide.bajo@gmail.com>
2021-10-20 14:18:41 +0200

    add style

--- index.php
+++ index.php
@@ -63,23 +63,30 @@ $db = [
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <!-- googleFonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
         <title>Document</title>
     </head>
     <body>
         <header>
-
+            <img src="img/googlelogo_clr_74x24px.svg" alt="">
         </header>       
         <main>
-            <div>
-				<?php foreach($db as $qr => $answer){ ?>
-					<h2><?php echo "{$answer['domanda']}"; ?></h2>
-                    <p><?php echo "{$answer['risposta']}"; ?></p>
-				<?php } ?>
-            </div>
-            <div>
-				<?php foreach($db as $qr => $answer){ ?>
-					
-				<?php } ?>
+            <div class="container">
+                <div>
+                    <?php foreach($db as $qr => $answer){ ?>
+                        <h2><?php echo "{$answer['domanda']}"; ?></h2>
+                        <p><?php echo "{$answer['risposta']}"; ?></p>
+                    <?php } ?>
+                </div>
+                <div>
+                    <?php foreach($db as $qr => $answer){ ?>
+                        
+                    <?php } ?>
+                </div>
             </div>
         </main>
 
